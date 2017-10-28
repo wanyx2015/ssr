6c73f652e31bd091a0c13dd914fc7853d11433a4
Wan <yuxi.wan@gmail.com>
2017-10-28 21:54:53 +0800

    generate random user info before creating user

--- add_ssr_user.sh
+++ add_ssr_user.sh
@@ -2,315 +2,42 @@
 
 #set -x
 
-check_sys(){
-	if [[ -f /etc/redhat-release ]]; then
-		release="centos"
-	elif cat /etc/issue | grep -q -E -i "debian"; then
-		release="debian"
-	elif cat /etc/issue | grep -q -E -i "ubuntu"; then
-		release="ubuntu"
-	elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
-		release="centos"
-	elif cat /proc/version | grep -q -E -i "debian"; then
-		release="debian"
-	elif cat /proc/version | grep -q -E -i "ubuntu"; then
-		release="ubuntu"
-	elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
-		release="centos"
-    fi
-	bit=`uname -m`
-}
+. ./libs.sh
 
-# 读取 配置信息
-Get_IP(){
-	ip=$(wget -qO- -t1 -T2 ipinfo.io/ip)
-	if [[ -z "${ip}" ]]; then
-		ip=$(wget -qO- -t1 -T2 api.ip.sb/ip)
-		if [[ -z "${ip}" ]]; then
-			ip=$(wget -qO- -t1 -T2 members.3322.org/dyndns/getip)
-			if [[ -z "${ip}" ]]; then
-				ip="VPS_IP"
-			fi
-		fi
-	fi
-}
+# 生成随机用户id
+userid=$[ ${RANDOM} % 10000 ]
+while [ ${userid} -lt 1000 ];
+do
+	userid=$(( ${RANDOM} % 10000 ))
+done
+echo userid 4 digits: ${userid}
 
-Add_iptables(){
-	if [[ ! -z "${ssr_port}" ]]; then
-		iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport ${ssr_port} -j ACCEPT
-		iptables -I INPUT -m state --state NEW -m udp -p udp --dport ${ssr_port} -j ACCEPT
-		ip6tables -I INPUT -m state --state NEW -m tcp -p tcp --dport ${ssr_port} -j ACCEPT
-		ip6tables -I INPUT -m state --state NEW -m udp -p udp --dport ${ssr_port} -j ACCEPT
-	fi
-}
+# 生成随机端口号
+ssr_port=$(( ${RANDOM} % 50000 ))
+while [ ${ssr_port} -lt 3000 ];
+do
+	ssr_port=$(( ${RANDOM} % 50000 ))
+done
+echo port 3000~50000: ${ssr_port}
 
+# 生成随机密码
+ssr_password_seed=$(( ${RANDOM} % 100000 ))
+while [ ${ssr_password_seed} -lt 10000 ];
+do
+	ssr_password_seed=$(( ${RANDOM} % 100000 ))
+done
+echo password seed in 5 digits: ${ssr_password_seed}
 
-Save_iptables(){
-	if [[ ${release} == "centos" ]]; then
-		service iptables save
-		service ip6tables save
-	else
-		iptables-save > /etc/iptables.up.rules
-		ip6tables-save > /etc/ip6tables.up.rules
-	fi
-}
-
-Del_iptables(){
-	port=$1
-	if [[ ! -z "${port}" ]]; then
-		iptables -D INPUT -m state --state NEW -m tcp -p tcp --dport ${port} -j ACCEPT
-		iptables -D INPUT -m state --state NEW -m udp -p udp --dport ${port} -j ACCEPT
-		ip6tables -D INPUT -m state --state NEW -m tcp -p tcp --dport ${port} -j ACCEPT
-		ip6tables -D INPUT -m state --state NEW -m udp -p udp --dport ${port} -j ACCEPT
-	fi
-}
-
-Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
-Info="${Green_font_prefix}[信息]${Font_color_suffix}"
-Error="${Red_font_prefix}[错误]${Font_color_suffix}"
-Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
-
-sh_ver="1.0.16"
-filepath=$(cd "$(dirname "$0")"; pwd)
-file=$(echo -e "${filepath}"|awk -F "$0" '{print $1}')
-ssr_folder="/usr/local/shadowsocksr"
-config_file="${ssr_folder}/config.json"
-config_user_file="${ssr_folder}/user-config.json"
-config_user_api_file="${ssr_folder}/userapiconfig.py"
-config_user_mudb_file="${ssr_folder}/mudb.json"
-ssr_log_file="${ssr_folder}/ssserver.log"
-Libsodiumr_file="/usr/local/lib/libsodium.so"
-Libsodiumr_ver_backup="1.0.15"
-Server_Speeder_file="/serverspeeder/bin/serverSpeeder.sh"
-LotServer_file="/appex/bin/serverSpeeder.sh"
-BBR_file="${file}/bbr.sh"
-jq_file="${ssr_folder}/jq"
-
-cd "${ssr_folder}"
-
-# 配置文件根目录：/usr/local/shadowsocksr
-
-# 用户配置文件：mudb.json
-
-
-
-Get_User_info(){
-	Get_user_port=$1
-	user_info_get=$(python mujson_mgr.py -l -p "${Get_user_port}")
-	match_info=$(echo "${user_info_get}"|grep -w "### user ")
-	if [[ -z "${match_info}" ]]; then
-		echo -e "${Error} 用户信息获取失败 ${Green_font_prefix}[端口: ${ssr_port}]${Font_color_suffix} " && exit 1
-	fi
-	user_name=$(echo "${user_info_get}"|grep -w "user :"|sed 's/[[:space:]]//g'|awk -F ":" '{print $NF}')
-	port=$(echo "${user_info_get}"|grep -w "port :"|sed 's/[[:space:]]//g'|awk -F ":" '{print $NF}')
-	password=$(echo "${user_info_get}"|grep -w "passwd :"|sed 's/[[:space:]]//g'|awk -F ":" '{print $NF}')
-	method=$(echo "${user_info_get}"|grep -w "method :"|sed 's/[[:space:]]//g'|awk -F ":" '{print $NF}')
-	protocol=$(echo "${user_info_get}"|grep -w "protocol :"|sed 's/[[:space:]]//g'|awk -F ":" '{print $NF}')
-	protocol_param=$(echo "${user_info_get}"|grep -w "protocol_param :"|sed 's/[[:space:]]//g'|awk -F ":" '{print $NF}')
-	[[ -z ${protocol_param} ]] && protocol_param="0(无限)"
-	obfs=$(echo "${user_info_get}"|grep -w "obfs :"|sed 's/[[:space:]]//g'|awk -F ":" '{print $NF}')
-	#transfer_enable=$(echo "${user_info_get}"|grep -w "transfer_enable :"|sed 's/[[:space:]]//g'|awk -F ":" '{print $NF}'|awk -F "ytes" '{print $1}'|sed 's/KB/ KB/;s/MB/ MB/;s/GB/ GB/;s/TB/ TB/;s/PB/ PB/')
-	#u=$(echo "${user_info_get}"|grep -w "u :"|sed 's/[[:space:]]//g'|awk -F ":" '{print $NF}')
-	#d=$(echo "${user_info_get}"|grep -w "d :"|sed 's/[[:space:]]//g'|awk -F ":" '{print $NF}')
-	forbidden_port=$(echo "${user_info_get}"|grep -w "forbidden_port :"|sed 's/[[:space:]]//g'|awk -F ":" '{print $NF}')
-	[[ -z ${forbidden_port} ]] && forbidden_port="无限制"
-	speed_limit_per_con=$(echo "${user_info_get}"|grep -w "speed_limit_per_con :"|sed 's/[[:space:]]//g'|awk -F ":" '{print $NF}')
-	speed_limit_per_user=$(echo "${user_info_get}"|grep -w "speed_limit_per_user :"|sed 's/[[:space:]]//g'|awk -F ":" '{print $NF}')
-	Get_User_transfer "${port}"
-}
-
-View_User_info(){
-	ip=$(cat ${config_user_api_file}|grep "SERVER_PUB_ADDR = "|awk -F "\'" '{print $2}')
-	[[ -z "${ip}" ]] && Get_IP
-	ss_ssr_determine
-	clear && echo "===================================================" && echo
-	echo -e " 用户 [${user_name}] 的配置信息：" && echo
-	echo -e " I  P\t    : ${Green_font_prefix}${ip}${Font_color_suffix}"
-	echo -e " 端口\t    : ${Green_font_prefix}${port}${Font_color_suffix}"
-	echo -e " 密码\t    : ${Green_font_prefix}${password}${Font_color_suffix}"
-	echo -e " 加密\t    : ${Green_font_prefix}${method}${Font_color_suffix}"
-	echo -e " 协议\t    : ${Red_font_prefix}${protocol}${Font_color_suffix}"
-	echo -e " 混淆\t    : ${Red_font_prefix}${obfs}${Font_color_suffix}"
-	echo -e " 设备数限制 : ${Green_font_prefix}${protocol_param}${Font_color_suffix}"
-	echo -e " 单线程限速 : ${Green_font_prefix}${speed_limit_per_con} KB/S${Font_color_suffix}"
-	echo -e " 用户总限速 : ${Green_font_prefix}${speed_limit_per_user} KB/S${Font_color_suffix}"
-	echo -e " 禁止的端口 : ${Green_font_prefix}${forbidden_port} ${Font_color_suffix}"
-	echo
-	echo -e " 已使用流量 : 上传: ${Green_font_prefix}${u}${Font_color_suffix} + 下载: ${Green_font_prefix}${d}${Font_color_suffix} = ${Green_font_prefix}${transfer_enable_Used_2}${Font_color_suffix}"
-	echo -e " 剩余的流量 : ${Green_font_prefix}${transfer_enable_Used} ${Font_color_suffix}"
-	echo -e " 用户总流量 : ${Green_font_prefix}${transfer_enable} ${Font_color_suffix}"
-	echo -e "${ss_link}"
-	echo -e "${ssr_link}"
-	echo -e " ${Green_font_prefix} 提示: ${Font_color_suffix}
- 在浏览器中，打开二维码链接，就可以看到二维码图片。
- 协议和混淆后面的[ _compatible ]，指的是 兼容原版协议/混淆。"
-	echo && echo "==================================================="
-}
-
-Get_User_transfer(){
-	transfer_port=$1
-	#echo "transfer_port=${transfer_port}"
-	all_port=$(${jq_file} '.[]|.port' ${config_user_mudb_file})
-	#echo "all_port=${all_port}"
-	port_num=$(echo "${all_port}"|grep -nw "${transfer_port}"|awk -F ":" '{print $1}')
-	#echo "port_num=${port_num}"
-	port_num_1=$(expr ${port_num} - 1)
-	#echo "port_num_1=${port_num_1}"
-	transfer_enable_1=$(${jq_file} ".[${port_num_1}].transfer_enable" ${config_user_mudb_file})
-	#echo "transfer_enable_1=${transfer_enable_1}"
-	u_1=$(${jq_file} ".[${port_num_1}].u" ${config_user_mudb_file})
-	#echo "u_1=${u_1}"
-	d_1=$(${jq_file} ".[${port_num_1}].d" ${config_user_mudb_file})
-	#echo "d_1=${d_1}"
-	transfer_enable_Used_2_1=$(expr ${u_1} + ${d_1})
-	#echo "transfer_enable_Used_2_1=${transfer_enable_Used_2_1}"
-	transfer_enable_Used_1=$(expr ${transfer_enable_1} - ${transfer_enable_Used_2_1})
-	#echo "transfer_enable_Used_1=${transfer_enable_Used_1}"
-	
-	
-	if [[ ${transfer_enable_1} -lt 1024 ]]; then
-		transfer_enable="${transfer_enable_1} B"
-	elif [[ ${transfer_enable_1} -lt 1048576 ]]; then
-		transfer_enable=$(awk 'BEGIN{printf "%.2f\n",'${transfer_enable_1}'/'1024'}')
-		transfer_enable="${transfer_enable} KB"
-	elif [[ ${transfer_enable_1} -lt 1073741824 ]]; then
-		transfer_enable=$(awk 'BEGIN{printf "%.2f\n",'${transfer_enable_1}'/'1048576'}')
-		transfer_enable="${transfer_enable} MB"
-	elif [[ ${transfer_enable_1} -lt 1099511627776 ]]; then
-		transfer_enable=$(awk 'BEGIN{printf "%.2f\n",'${transfer_enable_1}'/'1073741824'}')
-		transfer_enable="${transfer_enable} GB"
-	elif [[ ${transfer_enable_1} -lt 1125899906842624 ]]; then
-		transfer_enable=$(awk 'BEGIN{printf "%.2f\n",'${transfer_enable_1}'/'1099511627776'}')
-		transfer_enable="${transfer_enable} TB"
-	fi
-	#echo "transfer_enable=${transfer_enable}"
-	if [[ ${u_1} -lt 1024 ]]; then
-		u="${u_1} B"
-	elif [[ ${u_1} -lt 1048576 ]]; then
-		u=$(awk 'BEGIN{printf "%.2f\n",'${u_1}'/'1024'}')
-		u="${u} KB"
-	elif [[ ${u_1} -lt 1073741824 ]]; then
-		u=$(awk 'BEGIN{printf "%.2f\n",'${u_1}'/'1048576'}')
-		u="${u} MB"
-	elif [[ ${u_1} -lt 1099511627776 ]]; then
-		u=$(awk 'BEGIN{printf "%.2f\n",'${u_1}'/'1073741824'}')
-		u="${u} GB"
-	elif [[ ${u_1} -lt 1125899906842624 ]]; then
-		u=$(awk 'BEGIN{printf "%.2f\n",'${u_1}'/'1099511627776'}')
-		u="${u} TB"
-	fi
-	#echo "u=${u}"
-	if [[ ${d_1} -lt 1024 ]]; then
-		d="${d_1} B"
-	elif [[ ${d_1} -lt 1048576 ]]; then
-		d=$(awk 'BEGIN{printf "%.2f\n",'${d_1}'/'1024'}')
-		d="${d} KB"
-	elif [[ ${d_1} -lt 1073741824 ]]; then
-		d=$(awk 'BEGIN{printf "%.2f\n",'${d_1}'/'1048576'}')
-		d="${d} MB"
-	elif [[ ${d_1} -lt 1099511627776 ]]; then
-		d=$(awk 'BEGIN{printf "%.2f\n",'${d_1}'/'1073741824'}')
-		d="${d} GB"
-	elif [[ ${d_1} -lt 1125899906842624 ]]; then
-		d=$(awk 'BEGIN{printf "%.2f\n",'${d_1}'/'1099511627776'}')
-		d="${d} TB"
-	fi
-	#echo "d=${d}"
-	if [[ ${transfer_enable_Used_1} -lt 1024 ]]; then
-		transfer_enable_Used="${transfer_enable_Used_1} B"
-	elif [[ ${transfer_enable_Used_1} -lt 1048576 ]]; then
-		transfer_enable_Used=$(awk 'BEGIN{printf "%.2f\n",'${transfer_enable_Used_1}'/'1024'}')
-		transfer_enable_Used="${transfer_enable_Used} KB"
-	elif [[ ${transfer_enable_Used_1} -lt 1073741824 ]]; then
-		transfer_enable_Used=$(awk 'BEGIN{printf "%.2f\n",'${transfer_enable_Used_1}'/'1048576'}')
-		transfer_enable_Used="${transfer_enable_Used} MB"
-	elif [[ ${transfer_enable_Used_1} -lt 1099511627776 ]]; then
-		transfer_enable_Used=$(awk 'BEGIN{printf "%.2f\n",'${transfer_enable_Used_1}'/'1073741824'}')
-		transfer_enable_Used="${transfer_enable_Used} GB"
-	elif [[ ${transfer_enable_Used_1} -lt 1125899906842624 ]]; then
-		transfer_enable_Used=$(awk 'BEGIN{printf "%.2f\n",'${transfer_enable_Used_1}'/'1099511627776'}')
-		transfer_enable_Used="${transfer_enable_Used} TB"
-	fi
-	#echo "transfer_enable_Used=${transfer_enable_Used}"
-	if [[ ${transfer_enable_Used_2_1} -lt 1024 ]]; then
-		transfer_enable_Used_2="${transfer_enable_Used_2_1} B"
-	elif [[ ${transfer_enable_Used_2_1} -lt 1048576 ]]; then
-		transfer_enable_Used_2=$(awk 'BEGIN{printf "%.2f\n",'${transfer_enable_Used_2_1}'/'1024'}')
-		transfer_enable_Used_2="${transfer_enable_Used_2} KB"
-	elif [[ ${transfer_enable_Used_2_1} -lt 1073741824 ]]; then
-		transfer_enable_Used_2=$(awk 'BEGIN{printf "%.2f\n",'${transfer_enable_Used_2_1}'/'1048576'}')
-		transfer_enable_Used_2="${transfer_enable_Used_2} MB"
-	elif [[ ${transfer_enable_Used_2_1} -lt 1099511627776 ]]; then
-		transfer_enable_Used_2=$(awk 'BEGIN{printf "%.2f\n",'${transfer_enable_Used_2_1}'/'1073741824'}')
-		transfer_enable_Used_2="${transfer_enable_Used_2} GB"
-	elif [[ ${transfer_enable_Used_2_1} -lt 1125899906842624 ]]; then
-		transfer_enable_Used_2=$(awk 'BEGIN{printf "%.2f\n",'${transfer_enable_Used_2_1}'/'1099511627776'}')
-		transfer_enable_Used_2="${transfer_enable_Used_2} TB"
-	fi
-	#echo "transfer_enable_Used_2=${transfer_enable_Used_2}"
-}
-
-ss_ssr_determine(){
-	protocol_suffix=`echo ${protocol} | awk -F "_" '{print $NF}'`
-	obfs_suffix=`echo ${obfs} | awk -F "_" '{print $NF}'`
-	if [[ ${protocol} = "origin" ]]; then
-		if [[ ${obfs} = "plain" ]]; then
-			ss_link_qr
-			ssr_link=""
-		else
-			if [[ ${obfs_suffix} != "compatible" ]]; then
-				ss_link=""
-			else
-				ss_link_qr
-			fi
-		fi
-	else
-		if [[ ${protocol_suffix} != "compatible" ]]; then
-			ss_link=""
-		else
-			if [[ ${obfs_suffix} != "compatible" ]]; then
-				if [[ ${obfs_suffix} = "plain" ]]; then
-					ss_link_qr
-				else
-					ss_link=""
-				fi
-			else
-				ss_link_qr
-			fi
-		fi
-	fi
-	ssr_link_qr
-}
-urlsafe_base64(){
-	date=$(echo -n "$1"|base64|sed ':a;N;s/\n/ /g;ta'|sed 's/ //g;s/=//g;s/+/-/g;s/\//_/g')
-	echo -e "${date}"
-}
-ss_link_qr(){
-	SSbase64=$(urlsafe_base64 "${method}:${password}@${ip}:${port}")
-	SSurl="ss://${SSbase64}"
-	SSQRcode="http://doub.pw/qr/qr.php?text=${SSurl}"
-	ss_link=" SS    链接 : ${Green_font_prefix}${SSurl}${Font_color_suffix} \n SS  二维码 : ${Green_font_prefix}${SSQRcode}${Font_color_suffix}"
-}
-ssr_link_qr(){
-	SSRprotocol=$(echo ${protocol} | sed 's/_compatible//g')
-	SSRobfs=$(echo ${obfs} | sed 's/_compatible//g')
-	SSRPWDbase64=$(urlsafe_base64 "${password}")
-	SSRbase64=$(urlsafe_base64 "${ip}:${port}:${SSRprotocol}:${method}:${SSRobfs}:${SSRPWDbase64}")
-	SSRurl="ssr://${SSRbase64}"
-	SSRQRcode="http://doub.pw/qr/qr.php?text=${SSRurl}"
-	ssr_link=" SSR   链接 : ${Red_font_prefix}${SSRurl}${Font_color_suffix} \n SSR 二维码 : ${Red_font_prefix}${SSRQRcode}${Font_color_suffix} \n "
-}
-
-ssr_user=guest9999
-ssr_port=9999
-ssr_password=guest9999Pass
+ssr_user=guest${userid}
+ssr_port=${ssr_port}
+ssr_password=user${ssr_password_seed}
 ssr_method="aes-256-cfb"
 ssr_protocol="auth_sha1_v4"
 ssr_obfs="tls1.2_ticket_auth"
 ssr_protocol_param=""
 ssr_speed_limit_per_con=500
 ssr_speed_limit_per_user=600
-ssr_transfer=100
+ssr_transfer=50
 ssr_forbid=""
 
 check_sys
